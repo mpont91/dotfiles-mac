1fe3d2adc6ae38e5a66010db139cae32467cce1b
Marc Pont <mpont91@gmail.com>
2022-08-09 17:07:32 +0200

    editors vim configuration

--- setup.sh
+++ setup.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
+echo "⚙ Setting up editors configuration..."
+
+ln -sf ~/dotfiles/editors/vim/.vimrc ~/.vimrc
+echo "vim symlinks created successfully!"
+
 echo "⚙ Setting up git configuration..."
 
 ln -sf ~/dotfiles/git/.gitconfig ~/.gitconfig
-
 echo "git symlinks created successfully!"
